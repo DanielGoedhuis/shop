8d84bc8fe44fe96b8ac67f9bb971244bd649158f
DanielGoedhuis <77095626+DanielGoedhuis@users.noreply.github.com>
2021-12-16 09:55:41 +0100

    Update tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -3,6 +3,14 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
+    export const tile28 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile29 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile30 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile27 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
     export const tile9 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile11 = image.ofBuffer(hex``);
@@ -17,51 +25,43 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const tile23 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
+    export const tile26 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
     export const tile24 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile25 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile26 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile27 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile29 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile28 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile30 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
-            case "myTile7":
-            case "tile9":return tile9;
-            case "myTile9":
-            case "tile11":return tile11;
-            case "myTile12":
-            case "tile14":return tile14;
-            case "myTile13":
-            case "tile15":return tile15;
-            case "myTile19":
-            case "tile21":return tile21;
-            case "myTile20":
-            case "tile22":return tile22;
-            case "myTile21":
-            case "tile23":return tile23;
-            case "myTile22":
-            case "tile24":return tile24;
-            case "myTile23":
-            case "tile25":return tile25;
-            case "myTile24":
-            case "tile26":return tile26;
-            case "myTile25":
-            case "tile27":return tile27;
-            case "myTile27":
-            case "tile29":return tile29;
-            case "myTile26":
+            case "silver hasj":
             case "tile28":return tile28;
-            case "myTile28":
+            case "lemon hasj":
+            case "tile29":return tile29;
+            case "Sour diesel hasj":
             case "tile30":return tile30;
+            case "amnesia hasj":
+            case "tile27":return tile27;
+            case "lemon haze":
+            case "tile9":return tile9;
+            case "girl scout coockies":
+            case "tile11":return tile11;
+            case "ak-47":
+            case "tile14":return tile14;
+            case "amnesia haze":
+            case "tile15":return tile15;
+            case "og kush":
+            case "tile21":return tile21;
+            case "purple haze":
+            case "tile22":return tile22;
+            case "super silver haze":
+            case "tile23":return tile23;
+            case "critical hasj":
+            case "tile26":return tile26;
+            case "moroccan hasj":
+            case "tile24":return tile24;
+            case "Beldia hasj":
+            case "tile25":return tile25;
         }
         return null;
     })
